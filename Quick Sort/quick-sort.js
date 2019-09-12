6c2052771a049804b3744a745f5b81ce06dd1c1f
KuznetsovMA <jztenk@gmail.com>
2019-09-12 21:53:44 +0300

    Qick sort: num

--- Quick Sort/quick-sort.js
+++ Quick Sort/quick-sort.js
@@ -7,3 +7,14 @@ const sum = (array) => {
 }
 
 console.log(`Sum of array is ${sum([5, 4, 1, 3, 7])}`);
+
+
+const num = (array) => {
+    if (array.length === 0) {
+        return 0;
+    }
+
+    return 1 + num(array.slice(1));
+}
+
+console.log(`Nums in array is ${num([5, 4, 1, 3, 7])}`);

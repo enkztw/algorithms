dafaaea6d8c1a84f596a65580204a810fc42d42f
9MaxKuznetsov <jztenk@gmail.com>
2019-09-16 17:59:14 +0300

    Qick sort: maxNum & eslintrc es6 stt

--- Quick Sort/quick-sort.js
+++ Quick Sort/quick-sort.js
@@ -1,20 +1,32 @@
 const sum = (array) => {
-    if (array.length === 0) {
-        return 0;
-    }
+  if (array.length === 0) {
+    return 0;
+  }
 
-    return array.shift() + sum(array);
-}
+  return array.shift() + sum(array);
+};
 
 console.log(`Sum of array is ${sum([5, 4, 1, 3, 7])}`);
 
 
 const num = (array) => {
-    if (array.length === 0) {
-        return 0;
-    }
+  if (array.length === 0) {
+    return 0;
+  }
 
-    return 1 + num(array.slice(1));
-}
+  return 1 + num(array.slice(1));
+};
 
 console.log(`Nums in array is ${num([5, 4, 1, 3, 7])}`);
+
+const maxNum = (array) => {
+  if (array.length === 2) {
+    return array[0] > array[1] ? array[0] : array[1];
+  }
+
+  const subMaxNum = maxNum(array.slice(1));
+
+  return array[0] > subMaxNum ? array[0] : subMaxNum;
+};
+
+console.log(`Max num in array is ${maxNum([100, 5, 1, 10, 7, 4])}`);

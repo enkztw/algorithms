6e43cf19e0ce875f4fc96a92c5b338940e21e90c
9MaxKuznetsov <jztenk@gmail.com>
2019-09-18 18:16:55 +0300

    Qick sort: qsort

--- Quick Sort/quick-sort.js
+++ Quick Sort/quick-sort.js
@@ -6,7 +6,7 @@ const sum = (array) => {
   return array.shift() + sum(array);
 };
 
-console.log(`Sum of array is ${sum([5, 4, 1, 3, 7])}`);
+// console.log(`Sum of array is ${sum([5, 4, 1, 3, 7])}`);
 
 
 const num = (array) => {
@@ -17,7 +17,7 @@ const num = (array) => {
   return 1 + num(array.slice(1));
 };
 
-console.log(`Nums in array is ${num([5, 4, 1, 3, 7])}`);
+// console.log(`Nums in array is ${num([5, 4, 1, 3, 7])}`);
 
 const maxNum = (array) => {
   if (array.length === 2) {
@@ -29,4 +29,18 @@ const maxNum = (array) => {
   return array[0] > subMaxNum ? array[0] : subMaxNum;
 };
 
-console.log(`Max num in array is ${maxNum([100, 5, 1, 10, 7, 4])}`);
+// console.log(`Max num in array is ${maxNum([100, 5, 1, 10, 7, 4])}`);
+
+const qsort = (array) => {
+  if (array.length < 2) {
+    return array;
+  }
+
+  const pivot = array[0];
+  const less = array.filter((item) => item < pivot);
+  const greater = array.filter((item) => item > pivot);
+
+  return [...qsort(less), pivot, ...qsort(greater)];
+};
+
+console.log(`Sorted array is ${qsort([100, 5, 1, 10, 7, 4, 101, 102, 301, 404, 7, 19]).join(` `)}`);

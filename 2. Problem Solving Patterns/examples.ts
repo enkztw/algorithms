ec20b43fe1442c330d23a25679cfa8738a5169b6
KuznetsovMA <jztenk@gmail.com>
2020-05-05 16:51:21 +0300

    Problem understanding: concrete examples

---
+++ 2. Problem Solving Patterns/examples.ts
@@ -0,0 +1,15 @@
+// Write a function which takes in a string and returns counts of each character in the string
+
+// 1. Simple examples
+// countChars("aaaa") => {a: 4}
+// countChars("hello") => {h: 1, e: 1, l: 2, o: 1}
+
+// 2. Complex examples
+// countChars("my phone is 1841239") => spaces || numbers?
+// countChars("Hello and hi") => is "H" & "h" the same?
+
+// 3. Empty inputs
+// countChars("") => Error || {} || {a: 0, b: 0, ...}?
+
+// 4. Invalid inputs
+// countChars(undefined) => Error || {}?
